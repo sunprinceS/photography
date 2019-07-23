d9fb1a481edf08bc16481c49572ad909441515a5
sunprinceS <sunprince12014@gmail.com>
2019-07-23 10:03:26 +0800

    update package.json

--- npmfile.js
+++ npmfile.js
@@ -1,3 +1,3 @@
 exports.printMsg = function() {
-    console.log("Visit http://photography.ramswaroop.me for a treat!");
+    console.log("Visit http://127.0.0.1:4000/photography/ for a treat!");
 };
